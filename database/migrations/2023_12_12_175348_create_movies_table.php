5968c08f64087539e4c51148877d58d4bb689ca5
JISHNU TK <jishnutkglobaleyet@gmail.com>
2023-12-12 23:35:38 +0530

    added migrations pending

---
+++ database/migrations/2023_12_12_175348_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('genre', 50);
+            $table->date('release_date');
+            $table->string('language', 50);
+            $table->integer('duration');
+            $table->timestamps(); // Adds created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
---
+++ database/migrations/2023_12_12_175715_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->decimal('amount', 8, 2);
+            $table->datetime('booking_time');
+            $table->datetime('transaction_time');
+            $table->tinyInteger('status');
+            $table->string('payment_method', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
